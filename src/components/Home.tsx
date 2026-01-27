54183efd46701533f5979a0ca372a189d4982f77
Nitish C <86357181+Nitish-bot@users.noreply.github.com>
2026-01-28 01:27:59 +0530

    testing the waters with the homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,23 @@
+import { Sun, UserCircle } from '@untitledui/icons'
+
 export default function SignUp() {
-  return <div className="">Hello</div>
+  return (
+    <main className="mx-3 my-2">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <div>
+            <UserCircle className="size-8" strokeWidth={1.5} />
+          </div>
+          <div className="flex flex-col">
+            <div>John Smith</div>
+            <div>Aklasjjh1243</div>
+          </div>
+        </div>
+        <div>
+          <Sun />
+        </div>
+      </div>
+      <div>hello</div>
+    </main>
+  )
 }
